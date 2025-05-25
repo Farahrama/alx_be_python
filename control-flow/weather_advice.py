fcd563f82c6ecf2d1b116f1b24d066311bd9b70d
Farahrama <rafarah507@gmail.com>
2025-05-26 00:31:41 +0300

    wither

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+current_wither = input("What's the weather like today? (sunny/rainy/cold): ")
+if current_wither == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif current_wither == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif current_wither == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
