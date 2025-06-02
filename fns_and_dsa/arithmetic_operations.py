704d81314783e38a314ffd9b27bf166e120b1c51
Farahrama <rafarah507@gmail.com>
2025-06-02 15:32:03 +0300

    operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if num1 == 0:
+            if num2 == 0:
                 return "Cannot divide by zero"
             return num1 / num2
         case _:
