89d38545d460f0f61f54fc85431766f15586b5c6
Farahrama <rafarah507@gmail.com>
2025-06-14 04:13:23 +0300

    test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,17 +1,17 @@
 import unittest
-simplecalculator = __import__ ('simple_calculator').SimpleCalculator
+SimpleCalculator = __import__ ('simple_calculator').SimpleCalculator
 class Testsimplecalculator(unittest.TestCase):
     """Test cases for the SimpleCalculator class."""
     def test_add(self):
         """Test add method"""
-        self.assertEqual(simplecalculator().add(1, 2), 3)
+        self.assertEqual(SimpleCalculator().add(1, 2), 3)
     def test_multiply(self):
         """Test multiply method"""
-        self.assertEqual(simplecalculator().multiply(2, 3), 6)
+        self.assertEqual(SimpleCalculator().multiply(2, 3), 6)
     def test_subtract(self):
         """Test subtract method"""
-        self.assertEqual(simplecalculator().subtract(5, 3), 2)
+        self.assertEqual(SimpleCalculator().subtract(5, 3), 2)
     def test_divide(self):
         """Test divide method"""
-        self.assertEqual(simplecalculator().divide(6, 2), 3)
-        self.assertIsNone(simplecalculator().divide(6, 0))
+        self.assertEqual(SimpleCalculator().divide(6, 2), 3)
+        self.assertIsNone(SimpleCalculator().divide(6, 0))
