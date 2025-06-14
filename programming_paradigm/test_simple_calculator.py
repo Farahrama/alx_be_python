e27a03dcbfbbbe3272e49e0e2ccbcf987219d458
Farahrama <rafarah507@gmail.com>
2025-06-14 04:15:24 +0300

    test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from programming_paradigm.simple_calculator import SimpleCalculator
+from simple_calculator import SimpleCalculator
 class Testsimplecalculator(unittest.TestCase):
     """Test cases for the SimpleCalculator class."""
     def test_add(self):
