b6a084cfc7569cd2d17697d940f6b2867526f5e8
Farahrama <rafarah507@gmail.com>
2025-06-14 04:32:10 +0300

    test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,13 +6,13 @@ class Testsimplecalculator(unittest.TestCase):
     def test_addition(self):
         """Test addition 1, 2"""
         self.assertEqual(self.calc.add(1, 2), 3)
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test multiply method"""
         self.assertEqual(self.calc.multiply(2, 3), 6)
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test subtract method"""
         self.assertEqual(self.calc.subtract(5, 3), 2)
-    def test_divide(self):
+    def test_division(self):
         """Test divide method"""
         self.assertEqual(self.calc.divide(6, 2), 3)
         self.assertIsNone(self.calc.divide(6, 0))
